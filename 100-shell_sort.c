c129022268940d9e4947f7ddd7ac75ddc99cfb7a
Essaadia erraiss <128802580+saadiaerr@users.noreply.github.com>
2024-04-26 21:11:28 +0100

    Update 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort an array
+ *
+ * @array: array5667
+ * @size: size to 76
+ *
+ * Return: nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j, gap = 0;
+
+	if (size < 2)
+	{
+		return;
+	}
+
+	while ((gap = gap * 3 + 1) < size)
+		;
+
+	gap = (gap - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && temp <= array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
